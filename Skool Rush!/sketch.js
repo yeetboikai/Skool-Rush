ec703d228e244b9a30528df06f536fa642772fba
yeetboikai <43797546+yeetboikai@users.noreply.github.com>
2018-12-06 14:10:51 -0800

    Add files via upload

--- Skool Rush!/sketch.js
+++ Skool Rush!/sketch.js
@@ -1,81 +1,103 @@
-//create a variable to hold one ball
 let b;
 var value = 0;
 let testRight;
 let testLeft;
 let testUp;
 let testDown;
-// var monkey1, door, monkey2;
+var ref;
+var standing, walking;
+var timer;
+var timer1;
+var timerrunning=false;
+var door;
 
-// function preload(){
-//   monkey1 = loadImage('assets/monkey1.png');
-//   door = loadImage('assets/door.png');
-//   monkey2 = loadImage('assets/monkey2.png');
-// }
+function preload() {
+  standing = loadAnimation('assets/ref0001.png');
+  walking = loadAnimation('assets/ref0001.png','assets/ref0009.png');
+  door = loadImage('assets/door.png');
+}
 
 function setup() {
-  createCanvas(800, 400);
-  b = new Ball(400, 200); //make a new ball from the Ball class and call it b.
-  // imageMode (CENTER);
+  b = new Player(960, 540);
+  createCanvas(1920, 1080);
+  imageMode (CENTER);
 
 }
 
 
 function draw(){
+  text()
 	background(220);
   fill(0,0,0);
-  rect(100,400,400,50);
-  rect(800,400,50,50);
-    b.drawBall(); //draw the ball called b (go look in the Ball class for the drawBall function)
-    b.moveBall(); //move the ball called b (go look in the Ball class for the moveBall function)
+  rect(0,1070,1920,50);
+  rect(0,-40,1920,50);
+  rect(1910,0,50,1080);
+  rect(-40,0,50,1080);
+    b.drawPlayer();
+    b.movePlayer();
     b.detectWall();
-    // scale(1/4);
-    // image(door,200,300);
+    imageMode(CENTER);
+  scale(1/5);
+  image(door,4800,500);
 
+    // if(timerrunning==true){
+    //   timerdisplay();
+    // }
 }
 
+// function timerdisplay(){
+//   fill(0);
+//   text(frameCount,200,200);
+//   print((frameCount-timer1)/frameRate);
+// }
 
+class Player {
 
-//ball class from which to create new balls with similar properties.
-class Ball {
-
-	constructor(x,y){ //every ball needs an x value and a y value
+	constructor(x,y){
 		    this.x = x;
     		this.y = y;
 	}
-	drawBall(){  // draw a ball on the screen at x,y
-        fill(255, 255, 153);
-        noStroke();
-    		ellipse(this.x,this.y,50,50);
-	}
+	drawPlayer(){
+    ref = createSprite('assets/ref0001.png',this.x,this.y);
+    animation(standing,this.x,this.y);
+}
 
-
- moveBall(){ //update the location of the ball, so it moves across the screen
-   if (keyIsDown(LEFT_ARROW) && testLeft[0]>=200) {
-     this.x = this.x-2.5;
-     print("left");
-   } else if (keyIsDown(RIGHT_ARROW) && testRight[0]>=200) {
-     print("right");
-     this.x = this.x+2.5;
-   }
-   if (keyIsDown(UP_ARROW) && testUp[0]>=200) {
-     this.y = this.y-2.5;
-     print("up");
-   } else if (keyIsDown(DOWN_ARROW) && testDown[0]>=200) {
-     print("down");
-     this.y = this.y+2.5;
-   }
- }
+movePlayer(){
+  if (keyIsDown(LEFT_ARROW) && testLeft[0]>=200) {
+    this.x = this.x-2.5;
+    animation(walking, this.x, this.y);
+    print("left");
+  } else if (keyIsDown(RIGHT_ARROW) && testRight[0]>=200) {
+    print("right");
+    this.x = this.x+2.5;
+    animation(walking, this.x, this.y);
+  }
+  if (keyIsDown(UP_ARROW) && testUp[0]>=200) {
+    this.y = this.y-2.5;
+    print("up");
+    animation(walking, this.x, this.y);
+  } else if (keyIsDown(DOWN_ARROW) && testDown[0]>=200) {
+    print("down");
+    this.y = this.y+2.5;
+    animation(walking, this.x, this.y);
+  }
+}
 
  detectWall(){
 
-   testRight = get(this.x+25,this.y);
-   print(testRight);
-   testLeft = get(this.x-25, this.y);
-   print(testLeft);
-   testDown = get(this.x, this.y+25);
-   print(testDown);
-   testUp = get(this.x, this.y-25);
-   print(testUp);
+   testRight = get(this.x+82.5,this.y);
+   //print(testRight);
+   testLeft = get(this.x-85, this.y);
+   //print(testLeft);
+   testDown = get(this.x, this.y+51);
+   //print(testDown);
+   testUp = get(this.x, this.y-50);
+   //print(testUp);
   }
 }
+// function keyPressed(){
+//   if (keyIsDown(32)){
+//     timer1=frameCount;
+//     timerrunning=true;
+//   }
+// }
